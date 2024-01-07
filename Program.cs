255b6c89d111f8641ce7c0857eea6a685f3b0e33
janneyt <janneyt@oregonstate.edu>
2024-01-06 19:35:30 -0500

    create wizard page added

--- Program.cs
+++ Program.cs
@@ -1,11 +1,20 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using TNLOTR;
+using Microsoft.Extensions.Logging; // Add this namespace
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+// Add logging service
+builder.Logging.AddConfiguration(builder.Configuration.GetSection("Logging"));
+builder.Logging.AddConsole();
+builder.Logging.AddDebug();
+builder.Logging.AddEventSourceLogger();
+
+
 
 await builder.Build().RunAsync();
